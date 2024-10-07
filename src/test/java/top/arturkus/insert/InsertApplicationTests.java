c585a24bc991ca8468d5025e89f6b86286d5f00e
Artur Kuś <arturkus98@gmail.com>
2024-10-07 22:03:59 +0200

    Initial commit

---
+++ src/test/java/top/arturkus/insert/InsertApplicationTests.java
@@ -0,0 +1,13 @@
+package top.arturkus.insert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
